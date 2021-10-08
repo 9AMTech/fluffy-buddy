04ff08cd1f665565fc8e1f0379fe2ec02e571ff8
Ahmad M <48345582+9AMTech@users.noreply.github.com>
2021-10-07 20:40:39 -0400

    Finished Fluffy Buddy
    Completed the website page.

--- js/mainScript.js
+++ js/mainScript.js
@@ -82,84 +82,12 @@ const greetingMessage = AMPM + ' on this beautiful ' + dayOfTheWeek;
 document.getElementById('signUpGreeting').innerHTML = '<p>' + greetingMessage + '</p><p>' + date +'</p>';
 
 
-
-// Form Validation
-
-// Prevent the form from submitting
-function validateForm(submit) {
-    submit.preventDefault();
-}
-
-// Refactoring the message code
-function errorMessageUpdater(errorID) {
-    document.getElementById(errorID).textContent = message;
-}
-
-// Preventing Empty Fields AND Minimum 4 Character Checks
-function validation(myEvent) {
-    let element = myEvent.target;
-
-    // Username Validation
-    if(element.id == "username") {
-        let usernameValue = element.value;
-        if(usernameValue.length === 0)
-        {
-            message = "Username can't be blank!";
-            errorMessageUpdater('errorUsername');
-        }
-        else if(usernameValue.length > 0 && usernameValue.length < 4)
-        {
-            message = "Username length must be longer than 4 characters!";
-            errorMessageUpdater('errorUsername');
-        }
-        else{
-            message = "";
-            errorMessageUpdater('errorUsername');
-        }
-    }
-
-    // Password Validation
-    else if(element.id == "password") {
-        let passwordValue = element.value;
-        if(passwordValue.length === 0)
-        {
-            message = "Password can't be blank!";
-            errorMessageUpdater('errorPassword');
-        }
-        else if(passwordValue.length > 0 && passwordValue.length < 4)
-        {
-            message = "Username length must be longer than 4 characters!";
-            errorMessageUpdater('errorPassword');
-        }
-        else{
-            message = "";
-            errorMessageUpdater('errorPassword');
-        }
-    }
-
-    // Confirm Password Validation
-    else if(element.id == "confirmPassword") {
-        let confirmPasswordValue = element.value;
-        if(confirmPasswordValue.length === 0)
-        {
-            message = "Password Confirmation can't be blank!";
-            errorMessageUpdater('errorConfirmPassword');
-        }
-        else{
-            message = "";
-            errorMessageUpdater('errorConfirmPassword');
-        }
-    }
-}
-
-
-window.onload = function() {
-    var usernameInput = document.getElementById('username');
-    var passwordInput = document.getElementById('password');
-    var confirmPasswordInput = document.getElementById('confirmPassword');
-    document.getElementById('submitButton').addEventListener('submit', validateForm);
-    usernameInput.addEventListener("keyup", validation);
-    passwordInput.addEventListener("keyup", validation);
-    confirmPasswordInput.addEventListener("keyup", validation);
-}
-
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+// function myFunction() {
+//     var x = document.getElementById("hidden-links");
+//     if (x.style.display === "block") {
+//       x.style.display = "none";
+//     } else {
+//       x.style.display = "block";
+//     }
+//   }
